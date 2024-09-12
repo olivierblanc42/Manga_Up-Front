5da0583a6615ec4daf1b610b43176c5b0e078d38
SMARCOCCIA <seb@gmail.fr>
2024-09-12 18:53:18 +0200

    Ajout d'un console log pour la méthode currentDataUser

--- src/app/pages/Admin/user/user.component.ts
+++ src/app/pages/Admin/user/user.component.ts
@@ -78,7 +78,7 @@ export class UserComponent implements OnInit {
 
     this.userService.currentDataUser.subscribe(dataUser =>{
       this.dataUser = dataUser;
-      console.log(this.dataUser)
+      console.log("Dans admin > user.component : ", this.dataUser)
     })
 
   }
