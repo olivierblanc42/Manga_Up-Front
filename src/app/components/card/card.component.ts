92822042205df5cb3d8d71108307c6319b1c9244
SMARCOCCIA <seb@gmail.fr>
2024-09-12 18:49:43 +0200

    Ajout de class

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,54 +7,41 @@ import {Picture} from "../../types";
 import { ActivatedRoute } from '@angular/router';
 
 
-
 @Component({
-  selector: 'ui-card',
-  standalone: true,
-  imports: [NgClass],
-  template: `
-
-  <div class="card"
-  [ngClass]="{
-    'card-manga':size === 'card-manga',
+    selector: 'ui-card',
+    standalone: true,
+    imports: [NgClass],
+    template: `
+        <div 
+            class="card"
+            [ngClass]="{
+            'card-manga':size === 'card-manga',
+            'card-genre' :size  === 'card-genre',
+        }">
+            <ng-content></ng-content>
+        </div>
+    `,
+    styles: [`
+        .card-manga {
+            width: 167px;
+            height: 300px;
+        }
+        .card-manga-desktop {
+            width: 265px;
+            height: 431px;
+        }
     
-    'card-genre' :size  === 'card-genre',
-    
-}">
-    <ng-content></ng-content>
+        .card-genre {
+            width: 240px;
+            height: 146px;
+        }
 
-  </div>
-
-
-
-  `,
-  styles: [`
-
-  
-            
-         .card-manga {
-           width: 167px;
-           height: 300px;
-          }
-          .card-manga-desktop {
-           width: 265px;
-           height: 431px;
-          }
-     
-         .card-genre {
-           width: 240px;
-           height: 146px;
-          }
-
-          .card-gender-desktop {
-           width: 380px;
-           height: 268px;
-          }
-
-
+        .card-gender-desktop {
+            width: 380px;
+            height: 268px;
+        }
     `]
 })
 export class CardComponent {
-
   @Input() size: "desktop-manga" |  "card-genre" | "desktop-gender" | "card-manga" = "card-manga";
 }
